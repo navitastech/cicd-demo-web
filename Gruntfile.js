107ba40180d6be1517b0c1cc3270c80294da308d
steadystatecd <steadystatecd@gmail.com>
2017-07-25 00:31:21 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -428,41 +428,8 @@ module.exports = function (grunt) {
                         expand: true
                     }]
                 }
-            },
-            nexusDeployer: {
-                snapshot: {
-                    options: {
-                        groupId: 'com.navitas.demo.hello',
-                        artifactId: 'demo-web-ui',
-                        version: '<%= pkg.version %>.0-SNAPSHOT',
-                        packaging: 'tar',
-                        auth: {
-                            username: 'cicduser',
-                            password: 'admin123'
-                        },
-                        url: 'http://nexus.steadystatecd.com/repository/maven-snapshots/',
-                        artifact: 'dist/demo-web-ui-<%= pkg.version %>.0-<%= currentDate %>.tar',
-                        cwd: '/home/ec2-user/jenkins/data/workspace/web-build-on-commit/',
-                        insecure: true
-                    }
-                },
-                releaseCandidate: {
-                    options: {
-                        groupId: 'com.navitas.demo.hello',
-                        artifactId: 'demo-web-ui',
-                        version: '<%= pkg.version %>.0',
-                        packaging: 'tar',
-                        auth: {
-                            username: 'cicduser',
-                            password: 'admin123'
-                        },
-                        url: 'http://nexus.steadystatecd.com/repository/maven-releases/',
-                        artifact: 'dist/cts-web-ui-<%= pkg.version %>.0-<%= currentDate %>.tar',
-                        cwd: '/home/ec2-user/jenkins/data/workspace/web-build-on-commit/',
-                        insecure: true
-                    }
-                }
-            },
+     },
+            
 
     // Test settings
     karma: {
